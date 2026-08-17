6754471869463757f48bbdb724257e44fae88cb8
vriegert <95230893+zolkers@users.noreply.github.com>
2026-08-17 15:17:34 +0200

    feat: render desktop library with Compose

--- Anilib/Platforms/Desktop/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module fr.vriege.anilib.platform.desktop {
-    requires java.desktop;
-    requires fr.vriege.anilib.configuration.standard;
-    requires fr.vriege.anilib.feature.covercache.bundle;
-    requires fr.vriege.anilib.feature.library.api;
-    requires fr.vriege.anilib.kernel.api;
-}
