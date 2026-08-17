e88932ad19bdb5702dcd76b4195f9e086a2e9296
vriegert <95230893+zolkers@users.noreply.github.com>
2026-08-17 14:34:36 +0200

    build: wire library runtime consumers

--- Anilib/Features/Library/Bundle/src/main/java/module-info.java
+++ Anilib/Features/Library/Bundle/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module fr.vriege.anilib.feature.library.bundle {
     requires transitive fr.vriege.anilib.kernel.api;
     requires fr.vriege.anilib.feature.library.api;
-    requires fr.vriege.anilib.feature.library.core;
+    requires fr.vriege.anilib.feature.library.runtime;
 
     exports fr.vriege.anilib.feature.library.bundle;
 }
--- Anilib/Tooling/ArchitectureTests/src/main/java/module-info.java
+++ Anilib/Tooling/ArchitectureTests/src/main/java/module-info.java
@@ -4,4 +4,5 @@ module fr.vriege.anilib.tooling.archtests {
     requires fr.vriege.anilib.kernel.runtime;
     requires fr.vriege.anilib.configuration.standard;
     requires fr.vriege.anilib.feature.library.api;
+    requires fr.vriege.anilib.feature.library.runtime;
 }
