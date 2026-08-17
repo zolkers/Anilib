b51f684c5da657ec8685855de82af7b532aff5a3
vriegert <95230893+zolkers@users.noreply.github.com>
2026-08-17 14:24:57 +0200

    feat: add jdk desktop shell

---
+++ Anilib/Platforms/Desktop/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module fr.vriege.anilib.platform.desktop {
+    requires java.desktop;
+    requires fr.vriege.anilib.configuration.standard;
+    requires fr.vriege.anilib.feature.library.api;
+    requires fr.vriege.anilib.kernel.api;
+}
